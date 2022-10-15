aeeaf162fdf6012313ab520d6a291576b6406672
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2022-10-15 12:19:08 -0300

    adiciando alert ao finalizar a compra

--- Shopper-frontEnd/ecommecer/src/page/CartProduct/index.tsx
+++ Shopper-frontEnd/ecommecer/src/page/CartProduct/index.tsx
@@ -13,7 +13,7 @@ import Alert from '@mui/material/Alert';
 import IconButton from '@mui/material/IconButton';
 import Collapse from '@mui/material/Collapse';
 import Box from '@mui/material/Box';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { notify } from "../../services/notifyStyled";
 
@@ -107,7 +107,6 @@ export const CartProduct = (props: any) => {
                         >
                             Limpar carrinho
                         </Button>
-
                         <ToastContainer />
                     </Box>
                     : null
